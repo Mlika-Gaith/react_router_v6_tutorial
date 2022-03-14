c8cf30756c9490fa10b2776ddbbd5e1a60539a28
ghaith <mlika.gaith@gmail.com>
2022-03-14 12:29:04 +0100

    added navbar component and navigation links.

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.logo}>
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <h1>Book Keeper</h1>
+        </Link>
+      </div>
+      <div className={styles.nav_links}>
+        <ul>
+          <Link
+            to="/invoices"
+            style={{ color: "inherit", textDecoration: "none" }}
+          >
+            <li>Invoices</li>
+          </Link>
+
+          <Link
+            to="/expenses"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <li>Expenses</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+}
+export default Navbar;
